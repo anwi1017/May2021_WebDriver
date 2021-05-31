f0ffe98ba10215a801f9ba0762773921c5a17418
Ann Wilson <anwi1017@gmail.com>
2021-05-31 05:01:19 +0000

    updates

--- src/Test/WebDriverTest.java
+++ src/Test/WebDriverTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -31,9 +32,13 @@ public class WebDriverTest {
 		
 		UserName.sendKeys("anwi1017@mail.com");
 		
-		WebElement UserPwd = driver.findElement(By.name("user_pwd"));
+		WebElement Rememberme = driver.findElement(By.className("rememberMe"));
 		
-		UserPwd.sendKeys("9876");
+		Rememberme.click();
+		
+		WebElement Password = driver.findElement(By.id("password"));
+		
+		Password.sendKeys("9876");
 		
 		WebElement LoginBtn = driver.findElement(By.name("btn_login"));
 		
@@ -41,6 +46,36 @@ public class WebDriverTest {
 		
 		WebElement LoginError = driver.findElement(By.className("error_msg"));
 		
+		String ActMsg = LoginError.getText();
+		
+		String ClassName = LoginError.getAttribute("class");
+		String TagName = LoginError.getTagName();
+		
+		System.out.println("Class Name is "+ ClassName);
+		System.out.println("Tag Name is "+ TagName);
+		
+		String ExpMsg = "The email or password you have entered is invalid.";
+		
+		if(ActMsg.equals(ExpMsg)) {
+			System.out.println("Test Passed");
+		}else {
+			System.out.println("Test Failed");
+		}
+		
+		
+		List<WebElement> InputTags = driver.findElements(By.tagName("input"));
+		System.out.println("Total number of input tags are " + InputTags.size());
+		
+		List<WebElement> Links = driver.findElements(By.tagName("a"));
+		System.out.println("Total number of hyperlinks are " + Links.size());
+		
+		for(int index=0; index<Links.size(); index++) {
+		
+		System.out.println("Hyperlinks Text " + Links.get(index).getText());
+		
+		}
+		
+		driver.close();
 	
 		
 	}
